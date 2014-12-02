03ffebc372dffc191abdb24af9740b4ccccfaff4
dustan1 <goadustan@gmail.com>
2014-12-02 13:42:38 -0500

    fasdfe

---
+++ DamageTypes.h
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+struct resistanceTypes
+{
+   short magicResist;
+   short physicalResist;
+   short voidResist;
+
+   //Specific resistance types
+
+   //Magic resistances
+   short arcaneResist;
+   short fireMagicResist;
+   short electricMagicResist;
+
+
+
+
+};
+
+
+
+
+
+
