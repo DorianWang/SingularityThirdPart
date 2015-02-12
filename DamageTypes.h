ce58f2e28a4ae53f3ac891712cfbfd8fc26f72e3
dustan1 <goadustan@gmail.com>
2015-02-11 22:44:24 -0500

    ji

--- DamageTypes.h
+++ DamageTypes.h
@@ -9,20 +9,20 @@ struct resistanceTypes
    short magicResist;
    short physicalResist;
    short voidResist;
+   short otherResist;
 
    //Specific resistance types
 
    //Magic resistances
    short arcaneResist;
-   short fireMagicResist;
-   short electricMagicResist;
-   short freezeMagicResist;
+   short elementalMagicResist;
 
    //Physical resistances
    short stabResist;
    short slashResist;
    short crushResist;
-
+   short poisonResist;
+   short otherDOTResist;//Burning, freezing, etc.
 
 
 
