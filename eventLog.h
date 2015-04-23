6ee5206b2f7bf541891c88d6962435bff452f528
dustan1 <goadustan@gmail.com>
2015-04-23 11:44:38 -0400

    feasdf

--- eventLog.h
+++ eventLog.h
@@ -2,20 +2,33 @@
 #define EVENTLOG_H_INCLUDED
 
 #define EVENT_LOG_LOCATION "\Data\Event_logs"
+#define EVENT_LOG_NAME "STP_Log.txt"
 
-#define MAX_FILE_LINE_LENGTH 1024
+#define MAX_FILE_LINE_LENGTH 4096
+#define NORMAL_BUFFER_SIZE 8
 
 class eventLog
 {
 
 FileIO logFile;
-FileIO errorFile;
-
+std::string buffer;
 
 
 
 
 public:
+
+void addNewLog(std::string input)
+{
+   buffer << input << "\n" ;
+
+
+
+};
+
+
+bool pushBuffer();
+
 
 
 
